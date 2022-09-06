83ba90e4571e76743b2d959321e5ee774b316068
Andrei Varzar <43003757+wizzandrew@users.noreply.github.com>
2022-09-06 11:39:02 +0200

    reorganized menu,dish detail to functional components

--- frontend-lib-react/confusion/src/components/DishDetailComponent.jsx
+++ frontend-lib-react/confusion/src/components/DishDetailComponent.jsx
@@ -1,80 +1,82 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
 
-export default class DishDetail extends Component {
+function RenderDish({ dish }) {
 
-    renderDish(dish) {
-
-        if (dish != null) {
-            return (
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} width="100%" />
-                    <CardBody>
-                        <CardTitle tag="h5">{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
+    if (dish != null) {
+        return (
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} width="100%" />
+                <CardBody>
+                    <CardTitle tag="h5">{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
     }
+    else {
+        return (
+            <div></div>
+        );
+    }
+}
 
-    renderComments(dish) {
+function RenderComments({ dish }) {
 
-        if (dish != null && dish.comments != null) {
+    if (dish != null && dish.comments != null) {
 
-            let comments = dish.comments.map(com => {
-                return (
-                    <div>
-                        <p>{com.comment}</p>
-                        {/* <p>--{com.author}, {new Date(com.date).toDateString().substring(4)}</p> */}
-                        <p>--{com.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(com.date)))}</p>
-                    </div>
-                )
-            })
-
+        let comments = dish.comments.map(com => {
             return (
                 <div>
-                    <h4>Comments</h4>
-                    {comments}
+                    <p>{com.comment}</p>
+                    {/* <p>--{com.author}, {new Date(com.date).toDateString().substring(4)}</p> */}
+                    <p>--{com.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(com.date)))}</p>
                 </div>
             )
-
-        }
-
-        else {
-            return (
-                <div></div>
-            )
-        }
-    }
-
-    render() {
-
-        const data =
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.props.dish)}
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    {this.renderComments(this.props.dish)}
-                </div>
-            </div>
-
+        })
 
         return (
             <div>
-                <div className="container">
-                    {data}
-                </div>
-            </div >
+                <h4>Comments</h4>
+                {comments}
+            </div>
         )
+
     }
 
-
-
+    else {
+        return (
+            <div></div>
+        )
+    }
 }
+
+const DishDetail = (props) => {
+    const data =
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <RenderDish dish={props.dish} />
+            </div>
+            <div className="col-12 col-md-5 m-1">
+                <RenderComments dish={props.dish} />
+            </div>
+        </div>
+
+
+    return (
+        <div>
+            <div className="container">
+                {data}
+            </div>
+        </div >
+    )
+}
+
+export default DishDetail;
+
+
+
+
+
+
+
--- frontend-lib-react/confusion/src/components/MenuComponent.jsx
+++ frontend-lib-react/confusion/src/components/MenuComponent.jsx
@@ -1,31 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-
-export default class Menu extends Component {
-
-    render() {
-        const menu = this.props.dishes.map(
-            dish => {
-                return (
-                    <div className="col-12 col-md-5 m-1" key={dish.id}>
-                        <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
-                            <CardImg src={dish.image} alt={dish.name} width="100%" />
-                            <CardImgOverlay>
-                                <CardTitle tag="h5">{dish.name}</CardTitle>
-                            </CardImgOverlay>
-                        </Card>
-                    </div>
-                );
-            }
-        );
-
-        return (
-            <div className='container'>
-                <div className="row">
-                    {menu}
-                </div>
-            </div>
-        )
-    }
+function RenderMenuItem({ dish, onClick }) {
+    return (
+        <Card key={dish.id} onClick={() => onClick(dish.id)}>
+            <CardImg src={dish.image} alt={dish.name} width="100%" />
+            <CardImgOverlay>
+                <CardTitle tag="h5">{dish.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
 }
+
+const Menu = (props) => {
+    const menu = props.dishes.map(
+        dish => {
+            return (
+                <div className="col-12 col-md-5 m-1" key={dish.id}>
+                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                </div>
+            );
+        }
+    );
+
+    return (
+        <div className='container'>
+            <div className="row">
+                {menu}
+            </div>
+        </div>
+    )
+}
+
+export default Menu;
+
+
+
+
