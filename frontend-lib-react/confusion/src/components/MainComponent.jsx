0cffa84296fac36c72743fbe59beb727bf58e6ae
Andrei Varzar <43003757+wizzandrew@users.noreply.github.com>
2022-09-07 21:50:52 +0200

    spa part I

--- frontend-lib-react/confusion/src/components/MainComponent.jsx
+++ frontend-lib-react/confusion/src/components/MainComponent.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Routes, Route } from 'react-router-dom';
 
 
@@ -15,6 +19,9 @@ class MainComponent extends Component {
 
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
             // selectedDish: null
         };
     }
@@ -32,7 +39,10 @@ class MainComponent extends Component {
 
         const HomePage = () => {
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter(d => d.featured)[0]}
+                    promotion={this.state.promotions.filter(p => p.featured)[0]}
+                    leader={this.state.leaders.filter(l => l.featured)[0]}
+                />
             );
         }
 
@@ -43,6 +53,7 @@ class MainComponent extends Component {
                     <Route index element={<HomePage />} />
                     <Route path="/home" element={<HomePage />} />
                     <Route path="/menu" element={<Menu dishes={this.state.dishes} />} />
+                    <Route path="/contactus" element={<Contact />} />
                 </Routes>
                 <Footer />
             </div>
