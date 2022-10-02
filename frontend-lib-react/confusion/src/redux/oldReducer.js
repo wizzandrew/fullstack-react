b4e381527ed2e058c46667d944e745967b250a72
Andrei Varzar <43003757+wizzandrew@users.noreply.github.com>
2022-10-02 21:35:08 +0200

    combining reducers

---
+++ frontend-lib-react/confusion/src/redux/oldReducer.js
@@ -0,0 +1,11 @@
+// export const initialState = {
+//     dishes: DISHES,
+//     comments: COMMENTS,
+//     promotions: PROMOTIONS,
+//     leaders: LEADERS
+// };
+
+// //ES6 feature: if 'state' is undefined it will be assigned 'initialState'
+// export const Reducer = (state = initialState, action) => {
+//     return state;
+// };
